93f02e2af38a0d37836c9891ca2f9d001052d25a
Nayan2309 <nayanawalapane@gmail.com>
2026-09-21 12:04:55 +0530

    Add files via upload

---
+++ Case study_pract 9/script.js
@@ -0,0 +1,10 @@
+function showSchedule(day, time, subject) {
+
+    alert(
+        "Schedule Details\n\n" +
+        "Day: " + day + "\n" +
+        "Time: " + time + "\n" +
+        "Subject: " + subject
+    );
+
+}
